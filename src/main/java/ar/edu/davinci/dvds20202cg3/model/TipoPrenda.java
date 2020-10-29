211b0f1ff4481606c3ac8498999d0d7acbcbd0bd
GomezLucasGL <lucas.gomez1@davinci.edu.ar>
2020-10-28 23:13:25 -0300

    Prenda

---
+++ src/main/java/ar/edu/davinci/dvds20202cg3/model/TipoPrenda.java
@@ -0,0 +1,22 @@
+package ar.edu.davinci.dvds20202cg3.model;
+
+public enum TipoPrenda {
+
+    SACO("Saco"),
+    PANTALON("Pantalón"),
+    CAMISA("Camisa"),
+    CAMPERA("Campera"),
+    TAPADO("Tapado"),
+    CHAQUETA("Chaqueta");
+
+    private String descripcion;
+
+    TipoPrenda(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+}
+
