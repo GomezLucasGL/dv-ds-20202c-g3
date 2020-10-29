3484a76970975c7281864bf1cfc30afe0da5248b
GomezLucasGL <lucas.gomez1@davinci.edu.ar>
2020-10-28 21:48:44 -0300

    First commit

---
+++ src/test/java/ar/edu/davinci/dvds20202cg3/DvDs20202cG3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20202cg3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20202cG3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
